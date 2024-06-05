b4442aae400cde2a3e4d9e28dbdd60e1c01b8ec0
Sherzod <sherzodakramov1999@gmail.com>
2024-06-05 11:52:27 +0500

    leveling up

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,7 +32,8 @@ def run_game():
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)   
+        gf.check_events(ai_settings, screen, stats, play_button, ship, 
+                        aliens, bullets)   
         
         if stats.game_active:
             ship.update()
@@ -40,6 +41,6 @@ def run_game():
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
         gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets,
-            play_button)
+                         play_button)
         
 run_game()
